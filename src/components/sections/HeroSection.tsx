882abf561a9fa4724e9323f35ba496a4d3bfa7de
ernuyoga <ernuyoganirwana@gmail.com>
2026-09-17 12:22:53 +0700

    add file structures for landing page

---
+++ src/components/sections/HeroSection.tsx
@@ -0,0 +1,15 @@
+function HeroSection() {
+
+  return (
+    <section id='hero'>
+      <div className='left'>
+        <h1>Wujudkan Pernikahan Impianmu Bersama Kami</h1>
+      </div>
+      <div className='right'>
+        <h1>Gambar</h1>
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
---
+++ src/pages/landing/LandingPage.tsx
@@ -0,0 +1,10 @@
+import HeroSection from '../../components/sections/HeroSection.tsx';
+
+function LandingPage() {
+
+  return (
+    <HeroSection/>
+  )
+}
+
+export default LandingPage;
