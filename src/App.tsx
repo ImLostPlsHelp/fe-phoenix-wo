4ed0f70d1a8ecdd6f571d25f6dfcdfc6df3b120a
ImLostPlsHelp <maurits1005@gmail.com>
2026-09-18 03:27:21 +0700

    Nambah halaman login admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import LandingPage from './pages/landing/LandingPage.tsx';
+import Login from './pages/landing/Login.tsx';
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <LandingPage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/scp" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
