225d8bf73c04b9f5be8b76c708dd749522638d62
ernuyoga <ernuyoganirwana@gmail.com>
2026-09-17 14:45:30 +0700

    add section cards/promo

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,16 +1,18 @@
 import HeroSection from '../../components/sections/HeroSection.tsx';
 import Navbar from '../../components/layout/public/Navbar.tsx';
 import PartnerSection from '../../components/sections/PartnerSection.tsx';
+import CardsSection from '../../components/sections/CardsSection.tsx';
 
 function LandingPage() {
 
   return (
     <>
-    <Navbar/>
-    <HeroSection/>
-    <PartnerSection/>
+      <Navbar />
+      <HeroSection />
+      <PartnerSection />
+      <CardsSection />
     </>
-  )
+  );
 }
 
 export default LandingPage;
